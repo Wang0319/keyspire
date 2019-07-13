79e2ef04cd91abceb92924036093d8572af35da0
wqdragon <wqdragon@gmail.com>
2019-07-13 18:15:28 +0800

    first_cyslelf

--- application/views/login.php
+++ application/views/login.php
@@ -2,13 +2,13 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php base_url()?>assets/css/style.css">
-    <script type="text/javascript" src="<?php base_url()?>assets/js/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/style.css">
+    <script type="text/javascript" src="<?php echo base_url()?>assets/js/script.js"></script>
 </head>
-<body style="background: url('<?php base_url()?>assets/img/Splashscreen/splashscreen_prestige_background@2x.png') no-repeat; background-size: 100% 969px;">
+<body style="background: url('<?php echo base_url()?>assets/img/Splashscreen/splashscreen_prestige_background@2x.png') no-repeat; background-size: 100% 969px;">
     <div id="margin"></div>
     <div class="container col-lg-3 col-md-3 login_container">
-      <form action="<?php base_url()?>index.php/home" id="login_form" method="post">
+      <form action="<?php echo base_url()?>index.php/home" id="login_form" method="post">
         <div class="form-group">
           <input type="email" class="login_input form-control" placeholder="Enter email" name="email">
         </div>
--- application/views/side_bar.php
+++ application/views/side_bar.php
@@ -2,7 +2,20 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php base_url()?>assets/css/style.css">
-    <script type="text/javascript" src="<?php base_url()?>assets/js/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/style.css">
+    <script type="text/javascript" src="<?php echo base_url()?>assets/js/script.js"></script>
 </head>
 <body style="background-color: #282F7F">
+    <div id="side_bar">
+        <div>
+            <img src="<?php echo base_url();?>assets/img/Main Container/mc-menu-properties@2x.png" class="side_img" /><br />
+            <img src="<?php echo base_url();?>assets/img/Main Container/mc-menu-real-time@2x.png" class="side_img" /><br />
+            <img src="<?php echo base_url();?>assets/img/Main Container/mc-menu-portfolio@2x.png" class="side_img" /><br />
+            <img src="<?php echo base_url();?>assets/img/Main Container/mc-menu-potential@2x.png" class="side_img" /><br />
+            <img src="<?php echo base_url();?>assets/img/Main Container/mc-menu-mortgage@2x.png" class="side_img" />
+        </div>
+        <div id="side_bottom">
+            <img src="<?php echo base_url();?>assets/img/Main Container/mc-menu-about@2x.png" class="side_img" /><br />
+            <img src="<?php echo base_url();?>assets/img/Main Container/mc-menu-settings@2x.png" class="side_img" />
+        </div>
+    </div>
