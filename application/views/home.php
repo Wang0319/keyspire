edc838341269ea9b9907cb483a26e8952aa8200c
wqdragon <wqdragon@gmail.com>
2019-07-14 12:24:07 +0800

    right_content didn't finish

--- application/views/home.php
+++ application/views/home.php
@@ -1,18 +1,30 @@
-    <div class="left_content">
-        <div style="height: 10px; position: relative; background-color: #282F7F;"></div>
-        <div class="left_main">
-            <div class="left_top">
-                <span class="top_title">My Properties</span>
-                <a href="#">+</a>
+    <div class="content">
+        <div class="left_content">
+            <div style="height: 10px; position: relative; background-color: #282F7F;"></div>
+            <div class="left_main">
+                <div class="left_top">
+                    <span class="top_title">My Properties</span>
+                    <a href="#">+</a>
+                </div>
+                <div class="left_mid">
+                    <p>You do not currently have any<br /> properties.</p>
+                </div>
+                <img src="<?php echo base_url()?>assets/img/common-bottom-logo-panel@2x.png" style="position:      absolute; bottom: 10px; width: 350px; left: 0;" />
             </div>
-            <div class="left_mid">
-                <p>You do not currently have any<br /> properties.</p>
-            </div>
-            <img src="<?php echo base_url()?>assets/img/common-bottom-logo-panel@2x.png" style="position:   absolute; bottom: 0; width: 350px; left: 0;" />
+            <div style="height: 10px; bottom: 0px; position: absolute; background-color: #282F7F; width: 350px;"></div>
         </div>
-        <div style="height: 10px; bottom: 0px; position: absolute; background-color: #282F7F; width: 350px;"></div>
-    </div>
-    <div class="right_content">
-        
+        <div class="right_content">
+            <div style="height: 10px; position: relative; background-color: #282F7F;"></div>
+            <div class="right_main">
+                <div class="right_top">
+                    <span class="top_title">New Property</span>
+                    <a href="#">Done</a>
+                </div>
+                <div class="left_mid">
+                    <p>You do not currently have any<br /> properties.</p>
+                </div>
+            </div>
+            <div style="height: 10px; bottom: 0px; position: absolute; background-color: #282F7F; width: 350px;"></div>
+        </div>
     </div>
 </body>
--- application/views/side_bar.php
+++ application/views/side_bar.php
@@ -1,6 +1,6 @@
 <html lang="en">
 <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/style.css">
     <script type="text/javascript" src="<?php echo base_url()?>assets/js/script.js"></script>
