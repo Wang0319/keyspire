67b48b772096101444631261e46df01301df3a5c
wqdragon <wqdragon@gmail.com>
2019-07-17 03:30:21 +0800

    Revert "Revert "7.17""
    This reverts commit 365426ad97dc928afae5420f6cc9c564e662bb99.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,40 @@
-$(document).ready(function(){
-    $("#home_icon").click(function(){
-    	init_sidebar();
-        $("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
-    });
-    $("#cash_icon").click(function(){
-    	init_sidebar();
-        $("#cash_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
-    });
-    $("#portfolio_icon").click(function(){
-    	init_sidebar();
-        $("#portfolio_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
-    });
-    $("#potential_icon").click(function(){
-    	init_sidebar();
-        $("#potential_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
-    });
-    $("#calculator").click(function(){
-    	init_sidebar();
-        $("#calculator img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
-    });
-    $("#home_icon").click(function(){
-    	init_sidebar();
-        $("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
-    });
-    $("#home_icon").click(function(){
-    	init_sidebar();
-        $("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
-    });
-});
+// $(document).ready(function(){
+//     $("#home_icon").click(function(){
+//     	init_sidebar();
+//         $("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
+//     });
+//     $("#cash_icon").click(function(){
+//     	init_sidebar();
+//         $("#cash_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
+//     });
+//     $("#portfolio_icon").click(function(){
+//     	init_sidebar();
+//         $("#portfolio_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
+//     });
+//     $("#potential_icon").click(function(){
+//     	init_sidebar();
+//         $("#potential_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
+//     });
+//     $("#calculator_icon").click(function(){
+//     	init_sidebar();
+//         $("#calculator_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
+//     });
+//     $("#home_icon").click(function(){
+//     	init_sidebar();
+//         $("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
+//     });
+//     $("#home_icon").click(function(){
+//     	init_sidebar();
+//         $("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties-selected@2x.png");
+//     });
+// });
 
-function init_sidebar() {
-	$("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties@2x.png");
-	$("#cash_icon img").attr("src", "../assets/img/Main_Container/mc-menu-real-time@2x.png");
-	$("#portfolio_icon img").attr("src", "../assets/img/Main_Container/mc-menu-portfolio@2x.png");
-	$("#potential_icon img").attr("src", "../assets/img/Main_Container/mc-menu-potential@2x.png");
-	$("#calculator img").attr("src", "../assets/img/Main_Container/mc-menu-mortgage@2x.png");
-	$("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties@2x.png");
-	$("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties@2x.png");
-}
+// function init_sidebar() {
+// 	$("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties@2x.png");
+// 	$("#cash_icon img").attr("src", "../assets/img/Main_Container/mc-menu-real-time@2x.png");
+// 	$("#portfolio_icon img").attr("src", "../assets/img/Main_Container/mc-menu-portfolio@2x.png");
+// 	$("#potential_icon img").attr("src", "../assets/img/Main_Container/mc-menu-potential@2x.png");
+// 	$("#calculator_icon img").attr("src", "../assets/img/Main_Container/mc-menu-mortgage@2x.png");
+// 	$("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties@2x.png");
+// 	$("#home_icon img").attr("src", "../assets/img/main_container/mc-menu-properties@2x.png");
+// }
