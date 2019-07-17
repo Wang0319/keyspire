0f9612a01845549bdeb9ef01a2d17ea48d836257
WangQing <wqdragon0319@gmail.com>
2019-07-17 13:42:11 +0800

    t

---
+++ application/views/cash.php
@@ -0,0 +1,38 @@
+
+    <div class="left_content content">
+        <div class="top_title_container">
+            <span class="top_title">Real-time Cash Flow</span>
+        </div>
+        <div class="left_mid">
+            <p class="left_mid_space_title">You do not currently have any properties.</p>
+        </div>
+        <img src="<?php echo base_url()?>assets/img/common-bottom-logo-panel@2x.png" id="logo" />        
+    </div>
+    <div class="right_content col content">
+        <div class="top_title_container">
+        </div>
+        <div class="right_main">
+            <div class="container-fluid mt-3" style="padding: 0 10px;">
+                <div class="shadow-sm cash_date_bar" style="padding: 0;">
+					<a href="#home">Home</a>
+					<a href="#news">News</a>
+					<a href="#contact">Contact</a>
+					<a href="#about">About</a>
+					<a href="#support">Support</a>
+					<a href="#blog">Blog</a>
+					<a href="#tools">Tools</a>  
+					<a href="#base">Base</a>
+					<a href="#custom">Custom</a>
+					<a href="#more">More</a>
+					<a href="#logo">Logo</a>
+					<a href="#friends">Friends</a>
+					<a href="#partners">Partners</a>
+					<a href="#people">People</a>
+					<a href="#work">Work</a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
